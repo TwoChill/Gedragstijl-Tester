9e0d2502ed606fffddf517c26727d1be79303589
TwoChill <mdefance1988@gmail.com>
2023-08-27 19:24:12 +0200

    Toevoeging van functie om de dominante gedragsstijl te markeren in de plot op basis van de langste lijnlengte

--- main.py
+++ main.py
@@ -26,6 +26,13 @@ class BehaviorStyleTest:
         self.scores = {"O": 0, "S": 0, "I": 0, "D": 0}
         self.load_statements_from_config()
 
+    def print_scores(self):
+        print("Tally of scores:")
+        print(f"Denker (I): {self.scores['I']}")
+        print(f"Stabiel (S): {self.scores['S']}")
+        print(f"Dominant (D): {self.scores['D']}")
+        print(f"Relator (O): {self.scores['O']}")
+
     def load_statements_from_config(self):
         # Importing statements directly from the questions.py file
         for key, value in questions.Statements.items():
@@ -34,19 +41,18 @@ class BehaviorStyleTest:
             self.statements[int(key)] = (statements, styles)
 
     def run_test(self):
+        ax_number = 18
         question_number = 1
 
         print(intro_uitleg)
         input("Veel succes!\n\nDruk op Enter om te beginnen...")
-        clear_screen(intro_uitleg[289:])
+        clear_screen(intro_uitleg)
 
         selected_styles = {'I': 'RELATOR', 'S': 'SOCIAAL', 'D': 'DIRECTIEF', 'O': 'DENKER'}
-        style_colors = {'I': 'red', 'S': 'yellow', 'D': 'green', 'O': 'blue'}
-        axis_labels = {'I': 'Introvert', 'S': 'Stabiel', 'D': 'Dominant', 'O': 'Outgoing'}
+        style_colors = {'I': 'black', 'S': 'black', 'D': 'black', 'O': 'black'}
 
-        # Loop through statements in random order
         question_numbers = list(self.statements.keys())
-        random.shuffle(question_numbers)
+
         for question_num in question_numbers:
             statement_pair, styles = self.statements[question_num]
             formatted_statement_1 = statement_pair[0]
@@ -56,44 +62,84 @@ class BehaviorStyleTest:
             print(formatted_output)
             choice = input("Keuze (1/2): ")
             question_number += 1
-            clear_screen(intro_uitleg[289:])
+            clear_screen(intro_uitleg)
 
             try:
                 chosen_styles = styles.split(',')
-                for style in chosen_styles:
-                    self.scores[style] += 1
+                chosen_answer = int(choice) - 1
+                for index, style in enumerate(chosen_styles):
+                    if index == chosen_answer:
+                        self.scores[style] += 1
             except (ValueError, IndexError):
                 print("Ongeldige keuze. Kies 1 of 2.")
                 continue
 
+        ax_number = 10  # Change this value as needed
 
-        # Create the plot
-        plt.figure()
-        plt.xlabel('I')
-        plt.ylabel('S')
-        plt.plot([self.scores['I'], self.scores['O']], [self.scores['S'], self.scores['S']],
-                 color=style_colors['S'])
-        plt.plot([self.scores['I'], self.scores['I']], [self.scores['S'], self.scores['D']],
-                 color=style_colors['D'])
-        plt.plot([self.scores['O'], self.scores['O']], [self.scores['S'], self.scores['I']],
-                 color=style_colors['I'])
-        plt.plot([self.scores['O'], self.scores['I']], [self.scores['I'], self.scores['I']],
-                 color=style_colors['O'])
+        # Inner lines and labels
+        for i in range(1, ax_number):
+            plt.plot([i, -i], [0, 0], color='gray', linestyle='dotted', linewidth=0.5)
+            plt.plot([0, 0], [i, -i], color='gray', linestyle='dotted', linewidth=0.5)
 
-        # Highlight quadrant with highest score
-        max_style = max(self.scores, key=self.scores.get)
-        plt.fill([self.scores['I'], self.scores['O'], self.scores['O'], self.scores['I']],
-                 [self.scores['S'], self.scores['S'], self.scores['D'], self.scores['I']],
-                 color=style_colors[max_style], alpha=0.5)
+        # Quadrant lines (The Grid)
+        plt.plot([0, 0], [-ax_number, ax_number], color='black', linewidth=0.8)  # Vertical line at x=0
+        plt.plot([-ax_number, ax_number], [0, 0], color='black', linewidth=0.8)  # Horizontal line at y=0
 
-        # Add labels to quadrants
-        for style, (x, y) in self.scores.items():
-            plt.text(x, y, selected_styles[style], color=style_colors[style], ha='center', va='center')
+        # Calculate the maximum score and corresponding style
+        max_score = max(self.scores.values())
+        dominante_gedragsstijl = max(self.scores, key=self.scores.get)
 
-        plt.title("Gedragsstijl Grafiek")
-        plt.xlim(0, 18)
-        plt.ylim(0, 18)
+        # Determine the style with the longest line
+        longest_line_style = None
+        longest_line_length = 0
+        for style in self.scores:
+            if self.scores[style] == max_score:
+                if style == 'I':
+                    line_length = self.scores['I'] - self.scores['D']
+                elif style == 'O':
+                    line_length = self.scores['O'] - self.scores['S']
+                elif style == 'D':
+                    line_length = self.scores['D'] - self.scores['I']
+                elif style == 'S':
+                    line_length = self.scores['S'] - self.scores['O']
+
+                if line_length > longest_line_length:
+                    longest_line_length = line_length
+                    longest_line_style = style
+
+        # Add labels to quadrants with conditional font weight
+        for label, (key, x, y) in {'SOCIAAL': ('S', 0.6, 0.6), 'DIRECTIEF': ('D', -0.6, 0.6),
+                                   'DENKER': ('I', -0.6, -0.6), 'RELATOR': ('O', 0.6, -0.6)}.items():
+            weight = 'bold' if key == longest_line_style else 'normal'
+            plt.text(ax_number * x, ax_number * y, label, color='black', ha='center', va='center', fontweight=weight)
+
+        # Plot values of I, O, D, S against inner grid
+        plt.scatter(-self.scores['I'], 0)
+        plt.scatter(0, -self.scores['O'])
+        plt.scatter(self.scores['D'], 0)
+        plt.scatter(0, self.scores['S'])
+
+        # Connect the points in clockwise order
+        plt.plot([-self.scores['I'], 0, self.scores['D'], 0, -self.scores['I']],
+                 [0, -self.scores['O'], 0, self.scores['S'], 0],
+                 color='black', linestyle='-', linewidth=1)
+
+        # Title and axis limits
+        plt.title("Jouw gedragstijl is:")
+        plt.xticks([])  # Hide tick labels on x-axis
+        plt.yticks([])  # Hide tick labels on y-axis
+
+        # Add numbers to the middle of the grid lines
+        for i in range(1, ax_number):
+            plt.text(i, -0.5, str(i), color='black', ha='center', va='center')
+            plt.text(-i, -0.5, str(i), color='black', ha='center', va='center')
+            plt.text(-0.5, i, str(i), color='black', ha='center', va='center')
+            plt.text(-0.5, -i, str(i), color='black', ha='center', va='center')
+
+        plt.xlim(-ax_number, ax_number)
+        plt.ylim(-ax_number, ax_number)
         plt.show()
+
 
 if __name__ == "__main__":
     test = BehaviorStyleTest()
